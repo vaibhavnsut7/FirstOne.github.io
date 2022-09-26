210d6e1948a547bbd829819902248322629d91c4
vaibhavnsut7 <64123116+vaibhavnsut7@users.noreply.github.com>
2022-09-27 00:10:46 +0530

    first commit

--- Hello/settings.py
+++ Hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bgrqy^kdz)chy45i8i#wr_7-j0d(!8346#=^9kd(9yp1b*%j_a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ '127.0.0.1']
+ALLOWED_HOSTS = [ '127.0.0.1','firstonedjango.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
